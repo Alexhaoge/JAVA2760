ebc31aed1f91d7cd246290c4645c9c54a11f0832
Alexhaoge <alexhaoge@gmail.com>
2020-03-25 09:39:25 +0800

    字节流
    这个程序里运行时要加参数，vscode配置了一下

--- 输入输出流/字节流/TestNodeStreams.java
+++ 输入输出流/字节流/TestNodeStreams.java
@@ -1,22 +1,19 @@
-package 字节流;
-
 import java.io.*;
-
 public class TestNodeStreams{
-    public static void main(String[] args){
-        try{
-            FileReader input = new FileReader(args[0]);
-            FileWriter output = new FileWriter(args[1]);
-            char[] buffer = new char[128];
+    public static void main(final String[] args) {
+        try {
+            final FileReader input = new FileReader(args[0]);
+            final FileWriter output = new FileWriter(args[1]);
+            final char[] buffer = new char[128];
             int charsRead;
             charsRead = input.read(buffer);
-            while(charsRead != -1) {
+            while (charsRead != -1) {
                 output.write(buffer, 0, charsRead);
-                charsRead =  input.read(buffer);
+                charsRead = input.read(buffer);
             }
             input.close();
             output.close();
-        } catch (IOException e) {
+        } catch (final IOException e) {
             e.printStackTrace();
         }
     }
