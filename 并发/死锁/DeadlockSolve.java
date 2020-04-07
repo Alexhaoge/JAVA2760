1ea04f25647fce9f0eb1b7f0252ac506c6ff7292
Alexhaoge <alexhaoge@gmail.com>
2020-04-07 23:32:25 +0800

    死锁

---
+++ 并发/死锁/DeadlockSolve.java
@@ -0,0 +1,31 @@
+public class DeadlockSolve{
+    public static void main(String[] args){
+        final Object res1 = "resource1";
+        final Object res2 = "resource2";
+        Thread t1 = new Thread(){
+            public void run(){
+                synchronized(res1){
+                    System.out.println("Thread1: locked res1");
+                    try{Thread.sleep(50);}
+                    catch(InterruptedException e){}
+                }
+                synchronized(res2){
+                    System.out.println("Thread1: locked res2");
+                }
+            }
+        };
+        Thread t2 = new Thread(){
+            public void run(){
+                synchronized(res2){
+                    System.out.println("Thread2: locked res2");
+                    try{Thread.sleep(50);}
+                    catch(InterruptedException e){}
+                    synchronized(res1){
+                        System.out.println("Thread2: locked res1");
+                    }
+                }
+            }
+        };
+        t1.start();t2.start();
+    }
+}
